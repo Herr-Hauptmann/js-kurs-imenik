239a62d1a7eff7f7a0a262a1a6c93c867975597d
Tarik Horozovic <tarik.horoz@gmail.com>
2023-12-06 19:43:04 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,68 @@
+//Constants
+const ERROR_EMAIL = 1;
+const ERROR_CONTACT = 2;
+const SUCCESS = 3;
+
+let contactDatabase = [];
+
+
+//Listeners
+addContactBtn.addEventListener("click", addContactToDatabase);
+
+function validateInputs(contact){
+    for(key in contact)
+    {
+        if (!contact[key])
+        {
+            return key;
+        }
+    }
+    return false;
+}
+
+function checkIfContactInfoExists(email, phone){
+    for (const contact of contactDatabase)
+    {
+        if (contact.email === email)
+        {
+            return ERROR_EMAIL;
+        }
+        else if(contact.phone === phone)
+        {
+            return ERROR_CONTACT
+        }
+    }
+    return SUCCESS;
+}
+
+function addContactToDatabase(){
+    let newContact = {
+        name: nameInput.value,
+        surname: surnameInput.value,
+        email: emailInput.value,
+        phone: phoneInput.value,
+    };
+
+    let error = validateInputs(newContact);
+    if (error)
+    {
+        alert(`The field ${error} has an invalid value!`);
+        return;
+    }
+
+    error = checkIfContactInfoExists(newContact.email, newContact.phone);
+    switch(error){
+        case ERROR_EMAIL :
+            alert("User with this email exists!");
+            break;
+        case ERROR_CONTACT :
+            alert("User with this phone number exists!");
+            break;
+        default : 
+            alert("New user added!");
+            contactDatabase.push(newContact);
+            break;
+    }
+
+    console.log(contactDatabase);
+}
---
+++ assets/js/vendor.js
@@ -0,0 +1,6 @@
+//Html elements
+let addContactBtn = document.getElementById("addBtn");
+let nameInput = document.getElementById("name");
+let surnameInput = document.getElementById("surname");
+let emailInput = document.getElementById("email");
+let phoneInput = document.getElementById("phone");
